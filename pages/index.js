d1c6da5ef60ca76ae1f8a3a728134cadb1b75de2
ChrisAchinga <achinga.chris@gmail.com>
2020-11-15 21:09:00 +0300

    code lint

--- pages/index.js
+++ pages/index.js
@@ -1,35 +1,35 @@
-import Head from "next/head";
-import styles from "../styles/Home.module.css";
-import products from "../products.json";
+import Head from 'next/head'
+import styles from '../styles/Home.module.css'
+import products from '../products.json'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
         <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-        <link rel="preconnect" href="https://app.snipcart.com" />
-        <link rel="preconnect" href="https://cdn.snipcart.com" />
+        <link rel='icon' href='/favicon.ico' />
+        <link rel='preconnect' href='https://app.snipcart.com' />
+        <link rel='preconnect' href='https://cdn.snipcart.com' />
         <link
-          rel="stylesheet"
-          href="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css"
+          rel='stylesheet'
+          href='https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.css'
         />
       </Head>
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href='https://nextjs.org'>Next.js!</a>
         </h1>
 
         <p className={styles.description}>
           <a
-            className="snipcart-checkout snipcart-summary"
-            href="#"
-            style={{ textDecoration: "none" }}
+            className='snipcart-checkout snipcart-summary'
+            href='#'
+            style={{ textDecoration: 'none' }}
           >
-            <code className={styles.code}>pages/index.js</code>{" "}
-            <strong>Cart:</strong>{" "}
-            <span className="snipcart-total-price">$0.00</span>
+            <code className={styles.code}>pages/index.js</code>{' '}
+            <strong>Cart:</strong>{' '}
+            <span className='snipcart-total-price'>$0.00</span>
           </a>
         </p>
 
@@ -53,41 +53,41 @@ export default function Home() {
                 <p>${product.price}</p>
                 <p>
                   <button
-                    className="snipcart-add-item"
+                    className='snipcart-add-item'
                     data-item-id={product.id}
                     data-item-image={product.image}
                     data-item-name={product.title}
-                    data-item-url="/"
+                    data-item-url='/'
                     data-item-price={product.price}
                   >
                     Add to Cart
                   </button>
                 </p>
               </div>
-            );
+            )
           })}
         </div>
       </main>
 
       <footer className={styles.footer}>
         <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
+          href='https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app'
+          target='_blank'
+          rel='noopener noreferrer'
         >
-          Powered by{" "}
-          <img src="/vercel.svg" alt="Vercel Logo" className={styles.logo} />
+          Powered by{' '}
+          <img src='/vercel.svg' alt='Vercel Logo' className={styles.logo} />
         </a>
       </footer>
       <script
         async
-        src="https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js"
+        src='https://cdn.snipcart.com/themes/v3.0.21/default/snipcart.js'
       />
       <div
         hidden
-        id="snipcart"
-        data-api-key="MTZkMWI1ODMtMDQ4My00NGUxLTkyMjctMTEyMGMyMDQ5ZTYyNjM3MzU2MjE0MzMxMTgxMjk2"
+        id='snipcart'
+        data-api-key='MTZkMWI1ODMtMDQ4My00NGUxLTkyMjctMTEyMGMyMDQ5ZTYyNjM3MzU2MjE0MzMxMTgxMjk2'
       />
     </div>
-  );
+  )
 }
